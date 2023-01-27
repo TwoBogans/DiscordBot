655536a5ca2722bf8e9628417ca1d65cb4fdb595
RemainingToast <Remainingtoast@gmail.com>
2023-01-27 22:25:23 +1000

    fix bot info embed

--- src/main/java/org/au2b2t/commands/BotInfoCommand.java
+++ src/main/java/org/au2b2t/commands/BotInfoCommand.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -21,6 +22,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 public class BotInfoCommand extends CommandDataImpl {
 
@@ -36,10 +38,15 @@ public class BotInfoCommand extends CommandDataImpl {
         @Override
         public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
             if (event.getName().equalsIgnoreCase("botinfo")) {
-                var embed = new EmbedBuilder()
+                final var avatarUrl = "https://minotar.net/avatar/306eae875a8d4e148ec0375b54475d15/100.png";
+                final var embed = new EmbedBuilder()
+                        .setColor(2263842)
+                        .setThumbnail(avatarUrl)
                         .setTitle("Bot Information")
                         .addField("Uptime", Util.getUptime(), false)
-                        .setAuthor("Australian Hausemaster", "https://minotar.net/avatar/306eae875a8d4e148ec0375b54475d15/100.png")
+                        .addField("Total Guilds", String.valueOf(DiscordBot.getJda().getGuilds().size()), false)
+                        .addField("Total Members", String.valueOf(DiscordBot.getJda().getGuilds().stream().map(Guild::getMemberCount).collect(Collectors.toSet())), false)
+                        .setFooter("Australian Hausemaster", avatarUrl)
                         .setTimestamp(Instant.now())
                         .build();
 
