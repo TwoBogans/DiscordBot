5f3a0f324791b6cd4584f593e76aceff77eeb53b
RemainingToast <Remainingtoast@gmail.com>
2023-01-30 21:12:43 +1000

    update logo link

--- src/main/java/au/twobeetwotee/discord/util/Constants.java
+++ src/main/java/au/twobeetwotee/discord/util/Constants.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
     public static String WEBSITE = "https://2b2t.au";
-    public static String LOGO_URL = WEBSITE.concat("/2b2t_Australia_Logo.png");
+    public static String LOGO_URL = WEBSITE.concat("/New_2b2t_Australia_Logo.png");
     public static String LOGO_SQUARE_URL = WEBSITE.concat("/2b2t_Australia_Logo_Square.png");
     public static String ADMIN = "Australian Hausemaster";
     public static String ADMIN_URL = WEBSITE.concat("/")
