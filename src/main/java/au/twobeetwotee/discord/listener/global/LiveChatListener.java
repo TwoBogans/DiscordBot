669508ea9fe590eb17057c0dacb32bd8119ba40d
RemainingToast <Remainingtoast@gmail.com>
2023-01-30 22:41:13 +1000

    add live chat embed

--- src/main/java/au/twobeetwotee/discord/listener/global/LiveChatListener.java
+++ src/main/java/au/twobeetwotee/discord/listener/global/LiveChatListener.java
@@ -4,12 +4,14 @@ import au.twobbeetwotee.api.responses.ChatMessage;
 import au.twobeetwotee.discord.Main;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 public class LiveChatListener extends ListenerAdapter {
@@ -40,9 +42,28 @@ public class LiveChatListener extends ListenerAdapter {
                     if (entry.getValue() == null) continue;
                     if (entry.getKey() == latestHash) continue;
 
-                    System.out.println(response);
+                    var type = entry.getValue().getType();
+                    var bold = type.equalsIgnoreCase("join")
+                            || type.equalsIgnoreCase("quit")
+                            || type.equalsIgnoreCase("death");
 
-                    guildChannel.sendMessage(entry.getValue().getMessage()).queue();
+                    var msg = entry.getValue().getMessage();
+
+                    if (bold) {
+                        msg = msg.replaceAll("_", "\\_");
+                        msg = "**%s**".formatted(msg);
+                    } else {
+                        var who = msg.substring(msg.indexOf("<") + 1, msg.indexOf(">")).trim();
+                        var msg1 = msg.split(Pattern.quote("> "), 2)[1];
+                        who = who.replaceAll("_", "\\_");
+                        msg = "**<%s>** %s".formatted(who, msg1);
+                    }
+
+                    var embed = new EmbedBuilder()
+                            .setDescription(msg)
+                            .build();
+
+                    guildChannel.sendMessageEmbeds(embed).queue();
                     latestHash = entry.getKey();
                 }
             } catch (Exception e) {
